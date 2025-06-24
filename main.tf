b5e3c17bca6c949796232111e25d1e549578e41c
dev-kt07 <krishnatonk939@gmail.com>
2025-06-24 17:52:44 +0530

    adding some changes

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
 module "vpc" {
-  source = "./vpc"
-  variable "region" {}
+  source             = "./modules/vpc"
+  vpc_cidr           = "10.1.0.0/16"
+  public_subnets     = ["10.1.1.0/24", "10.1.2.0/24"]
+  private_subnets    = ["10.1.101.0/24", "10.1.102.0/24"]
+  availability_zones = ["us-east-2a", "us-east-2b"]
 }
