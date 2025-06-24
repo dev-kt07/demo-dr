0f3f363c64604b8b13f1314fd1541bfb010c0172
dev-kt07 <krishnatonk939@gmail.com>
2025-06-24 18:09:33 +0530

    adding some changes

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source             = "./modules/vpc"
+  source             = "./vpc"
   vpc_cidr           = "10.1.0.0/16"
   public_subnets     = ["10.1.1.0/24", "10.1.2.0/24"]
   private_subnets    = ["10.1.101.0/24", "10.1.102.0/24"]
